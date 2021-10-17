dd00e9126c1e9b4d97fdcc225b9b6094641077c8
DoodleBob <riley.foust18@gmail.com>
2021-10-16 22:20:48 -0400

    added generate markdown placeholder

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license.length === 0) {
+    return ""
+  } else {
+    return `![badge](https://img.shields.io/badge/license-${license}-blue.svg)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -8,17 +14,46 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `## License
+
+  ${renderLicenseBadge(license)}
+
+  ${renderLicenseLink(license)} 
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-  # ${data.projectDescription}
-  # ${data.installInfo}
-  # ${data.license}
-  # ${data.usageInfo}
+  return `
+  # ${data.title}
 
-`;
+  ## Description
+
+  ${data.projectDescription}
+
+  ## Table of Contents (Optional)
+
+  If your README is very long, add a table of contents to make it easy for users to find what they need.
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+
+  ## Installation
+
+  ${data.installInfo}
+
+  ## Usage 
+
+  ${data.usageInfo}
+
+  ${renderLicenseSection(data.license)}
+
+  ## Contributions
+
+  ${data.contributionsInfo}`;
 }
 
 module.exports = generateMarkdown;
